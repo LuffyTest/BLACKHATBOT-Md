affbc9044a072d11c550623a860da1d419fb493b
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-14 21:22:52 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,4 +1,4 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+let { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } require '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
     const results = await instagramdl(args[0])
