c7d591e185f903fb54c6c36f426b6f6065bce02a
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 16:51:43 +0530

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,29 +1,25 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm... where s the url?'
-let res = await fetch(API('https://single-developers.up.railway.app', '/tiktok', { url: text }, ))
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
   if (res.status !== 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  /*let url = json.server_1 || json.info[0].videoUrl || ''
-  if (!url) throw 'Gagal mengambil url download'
+  let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Failed to fetch download url'
   let txt = json.info[0].text
   for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
   await conn.sendFile(m.chat, url, 'tiktok.mp4', `
-▶ ${json.info[0].playCount} Views
-❤ ${json.info[0].diggCount} Likes
-🔁 ${json.info[0].shareCount} Shares
-💬 ${json.info[0].commentCount} Comments
-🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
+▶ Views: ${json.info[0].playCount} 
+❤ Likes: ${json.info[0].diggCount} 
+🔁 Shares: ${json.info[0].shareCount} 
+💬 Comments: ${json.info[0].commentCount} 
+🎵 Music Name: ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
 - *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
-- *Desc:*
-${txt}
-  `.trim(), m)*/
-  let url = json.no_watermark || json.result.link_dl2 || ''
-  if (!url) throw 'Failed to fetch download url'
-  let txt = `wm`
-    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
-}
+  `.trim(), m)
+
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
