a88d90a79b9dd3d5255aa50c16e9c5580f7a43ab
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 15:48:42 +0530

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -3,7 +3,7 @@ handler.before = async function (m) {
 
     if (m.isGroup && global.db.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.db.data.chats[m.chat].expired) {
-            this.reply(m.chat, `waktunya *${this.user.name}* untuk meninggalkan grup, mau bot ini stay di grup kamu? sewa ke nomor di bawah ini`, null).then(() => {
+            this.reply(m.chat, `time *${this.user.name}* to leave the group, want this bot to stay in your group? rent to the number below`, null).then(() => {
                 this.sendContact(m.chat, global.owner[0] + '@s.whatsapp.net', this.getName(global.owner[0] + '@s.whatsapp.net'), m).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         global.db.data.chats[m.chat].expired = 0
