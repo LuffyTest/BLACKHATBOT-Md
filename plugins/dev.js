fa731369329d67fac22c6301b5a84c2d31e3fbc3
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 16:13:18 +0530

    Update dev.js

--- plugins/dev.js
+++ plugins/dev.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  this.sendContact(m.chat, '62895369318181@s.whatsapp.net', this.getName('62895369318181@s.whatsapp.net'), m)
+  this.sendContact(m.chat, '94711392491@s.whatsapp.net', this.getName('94714519383@s.whatsapp.net'), m)
   //return m.reply (`Halo kak @${m.sender.split`@`[0]} ini nomor developer botku jangan diapa apain ya kak 😖`)
 }
 handler.help = ['creator']
