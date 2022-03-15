49491cb896bed3482a8e9ead9134000e3c36b08f
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 21:29:21 +0530

    Create downloader-ig.js

---
+++ plugins/downloader-ig.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (/^https?:\/\/.*(m.instagram.com||www.instagram.com|instagram.com)/i.test(m.text)) throw `only support url from ig and web only _please check url again_`
+
+  let res = await fetch(API('xteam', '/dl/igv2', { url: args[0] }, 'APIKEY'))
+  if (!res.ok) return m.reply(eror)
+  let json = await res.json()
+  await m.reply(wait)
+  await conn.sendFile(m.chat, json.result.sd.url, '', 'wm', m)
+}
+handler.help = ['fb'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+
+handler.premium = true
+
+module.exports = handler
