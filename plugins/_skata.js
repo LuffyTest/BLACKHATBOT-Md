7d54406a77ad542015baeb5948b3779f1ed4ad6a
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 15:56:02 +0530

    Update _skata.js

--- plugins/_skata.js
+++ plugins/_skata.js
@@ -7,28 +7,28 @@ handler.before = async function (m) {
     this.skata = this.skata ? this.skata : {}
     if (/nyerah/i.test(m.text) && (id in this.skata)) {
         delete conn.skata[id]
-        return await this.sendButton(m.chat, `Mulai lagi?`, wm, 'Sambung Kata', '.skata', m)
+        return await this.sendButton(m.chat, `Restart?`, wm, 'Connect Words', '.skata', m)
     }
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/(Mulai|Lanjut) :/i.test(m.quoted.text)) return !0
-    if (!(id in this.skata)) return await this.sendButton(m.chat, `Mulai lagi?`, wm, 'Sambung Kata', '.skata', m)
+    if (!(id in this.skata)) return await this.sendButton(m.chat, `Restart?`, wm, 'Connect Words', '.skata', m)
     if (m.quoted.id == this.skata[id][0].id) {
         let answerF = (m.text.toLowerCase().split` `[0]).trim()
         let res = await fetch(API('amel', '/ceksambungkata', { kata: m.text.toLowerCase().split` `[0] }, 'apikey'))
         if (!res.ok) throw eror
         let json = await res.json()
         if (!answerF.startsWith(this.filter(this.skata[id][1]))) {
-            return m.reply(`👎🏻 *Salah!*\nJawaban harus dimulai dari kata *${this.filter(this.skata[id][1]).toUpperCase()}*`)
+            return m.reply(`👎🏻 *Incorrect!*\nAnswers must start with the word *${this.filter(this.skata[id][1]).toUpperCase()}*`)
         } else if (!json.status) {
-            return m.reply(`👎🏻 *Salah!*\nKata *${m.text.toUpperCase()}* tidak valid!`)
+            return m.reply(`👎🏻 *Invalid!*\nWord *${m.text.toUpperCase()}* Invalid!`)
         } else if (this.skata[id][1] == answerF) {
-            return m.reply(`👎🏻 *Salah!*\nJawabanmu sama dengan soal, silahkan cari kata lain!`)
+            return m.reply(`👎🏻 *Wrong!*\nYour answer is the same as the question, please find another word!`)
         } else if (this.skata[id][2].includes(answerF)) {
-            return m.reply(`👎🏻 *Salah!*\nKata *${m.text.toUpperCase()}* sudah pernah digunakan!`)
+            return m.reply(`👎🏻 *Incorrect!*\nThe word *${m.text.toUpperCase()}* already used!`)
         }
         db.data.users[m.sender].exp += 100
         this.skata[id][2].push(answerF)
         this.skata[id] = [
-            await this.reply(m.chat, 'Lanjut : *' + answerF.toUpperCase() + '*\n\n*' + this.filter(answerF).toUpperCase() + '... ?*\n\n*balas pesan ini untuk menjawab!*', m),
+            await this.reply(m.chat, 'Carryon : *' + answerF.toUpperCase() + '*\n\n*' + this.filter(answerF).toUpperCase() + '... ?*\n\n*reply to this message to answer!*', m),
             answerF,
             this.skata[id][2]
         ]
