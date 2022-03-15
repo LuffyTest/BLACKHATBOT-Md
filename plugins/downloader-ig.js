7ca479988a32269c97503a700d9289b24a3a2c82
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 13:01:20 +0530

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,15 +1,14 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = await instagramdl(args[0])
-        .catch(async _ => await instagramdlv2(args[0]))
-        .catch(async _ => await instagramdlv3(args[0]))
-        .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
+let scraper = require('@bochilteam/scraper')
+
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw '𝚄𝙼𝙷...𝚆𝙴𝚁𝙴-𝙸𝚂-𝚃𝙷𝙴-𝚄𝚁𝙻?'
+  
+  let res = await scraper.instagramdl(args[0])
+  for (let i = 0; i < res.length; i++) await conn.sendFile(m.chat, res[i].url, '', '', m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
-handler.command = /^(ig(dl)?)$/i
+handler.limit = true
+handler.command = /^(ig(dl)|insta?)$/i
 
 module.exports = handler
