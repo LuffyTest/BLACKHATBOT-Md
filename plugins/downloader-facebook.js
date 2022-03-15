f9e2adfee31a4c92d01dbc1a4e7504171558ee30
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 13:14:19 +0530

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,14 +1,8 @@
-const fetch = require('node-fetch')
-let handler = async(m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. where is the url?\n\nexample:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  if (/^https?:\/\/.*(fb.watch|facebook.com)/i.test(m.text)) throw `wrong url`
-
-  let res = await fetch(API('neoxr', '/api/download/fb', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw error
-  let json = await res.json()
-  if (!json.status) throw json
-  await m.reply(wait)
-  await conn.sendFile(m.chat, json.data.sd.url, '', `HD: ${json.data.hd.url}\nSize: ${json.data.hd.size}\n\n © ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3`, m)
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
+    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
