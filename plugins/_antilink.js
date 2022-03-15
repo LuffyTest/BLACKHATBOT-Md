dbe7169ffebdff9a1d1dcc18d180b4580c756fb5
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 08:36:32 +0530

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,21 +1,26 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-export async function before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
+let handler = m => m
 
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-        if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) throw !0
-        }
-        conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, wm, ['off antilink', '/disable antilink'], m)
-        if (isBotAdmin && bot.restrict) {
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('Owner disable auto kick!')
-    }
-    return !0
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
+
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
+    await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
+    await m.reply(`*3!!*`)
+    await m.reply(`*2!!*`)
+    await m.reply(`*1!!*`)
+    if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
+    if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol.. enviaste el enlace de este grupo :v*')
+    await this.groupRemove(m.chat, [m.sender])
+  }
+  return true
 }
+
+module.exports = handler
