24362ceb7667822244347b075981baedfb2bfcbc
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 08:31:06 +0530

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,22 +1,21 @@
-let handler = m => m
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+export async function before(m, { isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = global.db.data.chats[m.chat]
+    let bot = global.db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
-  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
-
-  if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await this.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked out from this group byee!`)
-    if (isAdmin) return m.reply('*Hey sorry you\'re admin, you won\'t be kicked. haha..*')
-    if (!isBotAdmin) return m.reply('*Bot is not admin, how can it kick people -_-*')
-    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
-    let isLinkThisGc = new RegExp(linkGC, 'i')
-    let isgclink = isLinkThisGc.test(m.text)
-    if (isgclink) return m.reply('*:v*')
-    await this.groupRemove(m.chat, [m.sender])
-  }
-  return true
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) throw !0
+        }
+        conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, wm, ['off antilink', '/disable antilink'], m)
+        if (isBotAdmin && bot.restrict) {
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!bot.restrict) return m.reply('Owner disable auto kick!')
+    }
+    return !0
 }
-
-module.exports = handler
