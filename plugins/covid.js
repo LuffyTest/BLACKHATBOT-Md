dfd8078e4b087b535b8a98362258c42ca4b5c60f
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-15 16:16:33 +0530

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -5,7 +5,7 @@ let handler = async (m, { text }) => {
   let json = await res.json()
   if (!json.confirmed) throw 'Negara?'
   if (json.confirmed) m.reply(`
-Countries : ${text}
+Country : ${text}
 Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
@@ -13,7 +13,7 @@ Last Update : ${json.lastUpdate}
 \n\n2022 ${global.wm}`.trim())
   else throw json
 }
-handler.help = ['covid'].map(v => v + ' <negara>')
+handler.help = ['covid'].map(v => v + ' <country>')
 handler.tags = ['internet']
 handler.command = /^(corona|covid|covid19)$/i
 //susu, tetek, oppai
