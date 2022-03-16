4280db9252dfe100dff2594e23bed361bbeceda2
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-16 15:54:13 +0530

    Update filebokep.js

--- plugins/filebokep.js
+++ plugins/filebokep.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn }) => {
 	
-	await m.reply('Si ajg mau coli.')
+	await m.reply('also wants to have a bite.')
   conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
 }
 handler.help = ['filebokep']
