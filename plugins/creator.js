f04bba36211e39e6d4d92487b8b75256b1e301c5
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-16 13:10:57 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ const delay = time => new Promise(res => setTimeout(res, time))
 let handler = async(m, { conn }) => {
   conn.sendContact(m.chat, global.owner[0] + '@s.whatsapp.net', conn.getName(global.owner[0] + '@s.whatsapp.net'), m)
   await delay(500)
-  conn.reply(m.chat, `Halo kak ${await conn.getName(m.sender)} itu nomor ownerku jangan di apa-apain ya kak😖`, m)
+  conn.reply(m.chat, `Hello sis ${await conn.getName(m.sender)} that's my owner's number, don't worry about it,`, m)
 }
 handler.help = ['owner']
 handler.tags = ['info']
