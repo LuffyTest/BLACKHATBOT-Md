d8c88dced750d920287a24dac5897033938357ce
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-16 14:00:58 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -2,7 +2,7 @@ let { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys-md
 let limit = 50
 const { servers, yta } = require('../lib/y2mate')
 let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... where\'s the url?', m)
     let chat = global.db.data.chats[m.chat]
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
